c59fcf5a62b306394d06f3b2d5656fe962c5c6cd
Gerrit Kitts <gkitts@gmail.com>
2019-02-01 18:47:41 -0500

    Removing some unnecessary logging.

--- IRBT14MadlibsFix/IRBT14MadlibsFix/Patches/Patches.cs
+++ IRBT14MadlibsFix/IRBT14MadlibsFix/Patches/Patches.cs
@@ -18,8 +18,6 @@ namespace IRBT14MadlibsFix.Patches {
         public static bool Prefix(TeamOverride __instance, Contract contract, DataManager dataManager, 
             ref Contract ___contract, ref DataManager ___dataManager, ref string ___teamLeaderCastDefId, ref List<LanceOverride> ___lanceOverrideList, ref Faction ___faction) {
 
-            LogDMFactions(dataManager);
-
             ___contract = contract;
             ___dataManager = dataManager;
 
@@ -30,20 +28,11 @@ namespace IRBT14MadlibsFix.Patches {
             for (int i = 0; i < ___lanceOverrideList.Count; ++i) {
                 LanceOverride lanceOverride = ___lanceOverrideList[i];
                 lanceOverrides[lanceOverride.GetHashCode()] = __instance;
-                IRBT14MadlibsFix.Logger.Log($"TO:RML setting teamOverride:{__instance.GetHashCode()} for lanceOverride:{lanceOverride.GetHashCode()}");
+                IRBT14MadlibsFix.Logger.Log($"TO:RML setting teamOverride:[{__instance.GetHashCode()}] for lanceOverride:[{lanceOverride.GetHashCode()}]");
                 lanceOverride.RunMadLibs(contract);
             }
 
             return false;
-        }
-
-        private static void LogDMFactions(DataManager dataManager) {
-            if (!HasLoggedFactions) {
-                foreach (KeyValuePair<string, FactionDef> kvp in dataManager.Factions) {
-                    IRBT14MadlibsFix.Logger.Log($"{kvp.Key} has factionDef:{kvp.Value} with name:{kvp.Value?.Name} and denonym:{kvp.Value?.Demonym}");
-                }
-                HasLoggedFactions = true;
-            }
         }
     }
 
@@ -55,13 +44,13 @@ namespace IRBT14MadlibsFix.Patches {
             ref TagSet ___lanceTagSet, ref TagSet ___lanceExcludedTagSet, ref List<UnitSpawnPointOverride> ___unitSpawnPointOverrideList) {
 
             TeamOverride teamOverride = TeamOverride_RunMadLibs.lanceOverrides[__instance.GetHashCode()];
-            IRBT14MadlibsFix.Logger.Log($"LO:RML using teamOverride:{teamOverride.GetHashCode()} for lanceOverride:{__instance?.GetHashCode()}");
+            IRBT14MadlibsFix.Logger.Log($"LO:RML using teamOverride:[{teamOverride.GetHashCode()}] for lanceOverride:[{__instance?.GetHashCode()}]");
             LanceOverride_RunMadLibs.teamOverrides[__instance.GetHashCode()] = teamOverride;
             contract.GameContext.SetObject(GameContextObjectTagEnum.CurrentTeam, teamOverride);
 
-            IRBT14MadlibsFix.Logger.Log($"LO:RML BEFORE madlibs lanceTagSet is:{___lanceTagSet}");
+            IRBT14MadlibsFix.Logger.Log($"LO:RML BEFORE madlibs lanceTagSet is:[{___lanceTagSet}]");
             contract.RunMadLib(___lanceTagSet);
-            IRBT14MadlibsFix.Logger.Log($"LO:RML AFTER madlibs lanceTagSet is:{___lanceTagSet}");
+            IRBT14MadlibsFix.Logger.Log($"LO:RML AFTER madlibs lanceTagSet is:[{___lanceTagSet}]");
             contract.RunMadLib(___lanceExcludedTagSet);
 
             for (int i = 0; i < ___unitSpawnPointOverrideList.Count; ++i) {
@@ -76,7 +65,7 @@ namespace IRBT14MadlibsFix.Patches {
     public static class LanceOverride_RequestLance {
 
         public static bool Prefix(LanceOverride __instance, Contract contract) {
-            IRBT14MadlibsFix.Logger.Log($"LO:RL invoked with contract:{contract} with GUID:{contract?.GUID}");
+            IRBT14MadlibsFix.Logger.Log($"LO:RL invoked with contract:[{contract}] with GUID:[{contract?.GUID}]");
             return true;
         }
     }
@@ -89,12 +78,11 @@ namespace IRBT14MadlibsFix.Patches {
                 new Type[] { typeof(Contract), typeof(LanceDef) });
         }
 
-        public static bool Prefix(LanceOverride __instance, Contract contract, LanceDef lanceDef) {
-            IRBT14MadlibsFix.Logger.Log($"LO:RMLOLD - entered.");
+        public static bool Prefix(LanceOverride __instance, Contract contract, LanceDef lanceDef) {            
             if (contract != null) {
                 // Setup CurrentTeam before running the madlibs
                 TeamOverride teamOverride = LanceOverride_RunMadLibs.teamOverrides[__instance.GetHashCode()];
-                IRBT14MadlibsFix.Logger.Log($"LO:RMLOLD using teamOverride:{teamOverride.GetHashCode()} for lanceOverride:{__instance?.GetHashCode()}");
+                IRBT14MadlibsFix.Logger.Log($"LO:RMLOLD using teamOverride:[{teamOverride.GetHashCode()}] for lanceOverride:[{__instance?.GetHashCode()}]");
 
                 contract.GameContext.SetObject(GameContextObjectTagEnum.CurrentTeam, teamOverride);
                 contract.RunMadLib(lanceDef.LanceTags);
@@ -103,9 +91,9 @@ namespace IRBT14MadlibsFix.Patches {
                     Traverse ensureTagSetsT = Traverse.Create(unit).Method("EnsureTagSets");
                     ensureTagSetsT.GetValue();
 
-                    IRBT14MadlibsFix.Logger.Log($"LO:RMLOLD BEFORE madlibs lanceTagSet is:{unit.unitTagSet}");
+                    IRBT14MadlibsFix.Logger.Log($"LO:RMLOLD BEFORE madlibs lanceTagSet is:[{unit.unitTagSet}]");
                     contract.RunMadLib(unit.unitTagSet);
-                    IRBT14MadlibsFix.Logger.Log($"LO:RMLOLD AFTER madlibs lanceTagSet is:{unit.unitTagSet}");
+                    IRBT14MadlibsFix.Logger.Log($"LO:RMLOLD AFTER madlibs lanceTagSet is:[{unit.unitTagSet}]");
 
                     contract.RunMadLib(unit.excludedUnitTagSet);
                     contract.RunMadLib(unit.pilotTagSet);
@@ -114,21 +102,6 @@ namespace IRBT14MadlibsFix.Patches {
             }
 
             return false;
-        }
-    }
-
-
-    [HarmonyPatch]
-    public static class Interpolator_GetObjectByStringFromObject {
-
-        public static MethodInfo TargetMethod() {
-            return AccessTools.Method(typeof(Interpolator), "GetObjectByStringFromObject",
-                new Type[] { typeof(string), typeof(object) });
-        }
-        
-        public static bool Prefix(string expr, object obj) {
-            IRBT14MadlibsFix.Logger.Log($"I:GOBSFO entered with expr:{expr} and obj:{obj}");
-            return true;
         }
     }
 
